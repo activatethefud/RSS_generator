5da53bd0f34393bd3272bedfcf9de700ec1b3cac
nsutic <nikolasutic1@gmail.com>
2019-06-02 23:25:48 +0200

    Spacing.

--- generator.py
+++ generator.py
@@ -42,15 +42,6 @@ This is how it should look. When all of the arguments are set, and the program
 runs, it will create the .xml feed file, with no whitespaces in the name.  '''
 
 
-if len(sys.argv) == 2 and sys.argv[1] == '-':
-	read_from_stdin_flag=True
-	stdin_lines=[]
-	for stdin_line in sys.stdin.readlines():
-		stdin_lines.append(stdin_line.rstrip('\n'))
-	num_of_input_lines=len(stdin_lines)
-else:
-	read_from_stdin_flag=False
-
 class Generator:
 	def __init__(self,channel_title=None,channel_link=None,channel_description=None):
 		''' This sets up the generator. The archived article titles and
@@ -58,21 +49,27 @@ class Generator:
 		are present, they will be created. The channel title, link, and
 		description is set. The browser opens and waits for the
 		javascript to execute, and load the newly made html. '''
+
 		titles=open("titles","r")
 		links=open("links","r")
 		self.archived_titles=[]
 		self.archived_links=[]
+
 		for archived_title in titles.readlines():
 			self.archived_titles.append(archived_title.rstrip('\n'))
+
 		for archived_link in links.readlines():
 			self.archived_links.append(archived_link.rstrip('\n'))
+
 		self.channel_title=channel_title
 		self.channel_link=channel_link
 		self.channel_description=channel_description
+
 		options=webdriver.FirefoxOptions()
 		options.add_argument("--headless")
 		self.browser=webdriver.Firefox(options=options)
 		self.browser.get(self.channel_link)
+
 		links.close()
 		titles.close()
 	
@@ -82,24 +79,33 @@ class Generator:
 		description in two paragraph elements for whatever reason. If
 		this is the case, I encourage you to edit the code yourself, or
 		wait for the feature to get coded, lmao. '''
+
 		self.new_titles=[]
 		self.new_links=[]
 		self.new_descriptions=[]
+
 		if read_from_stdin_flag is True:
+
 			title_selector=stdin_lines[3]
 			link_selector=stdin_lines[4]
+
 			if num_of_input_lines == 6:
+
 				description_selector=stdin_lines[5]
 		else:
+
 			title_selector=input("Enter the title CSS selector: ")
 			link_selector=input("Enter the link CSS selector: ")
 			description_selector=input("Enter description CSS selector: ")
+
 		for title in self.browser.find_elements_by_css_selector(title_selector):
 			if title.text not in self.archived_titles:
 				self.new_titles.append(title.text)
+
 		for link in self.browser.find_elements_by_css_selector(link_selector):
 			if link.get_attribute("href") not in self.archived_links:
 				self.new_links.append(link.get_attribute("href"))
+
 		if num_of_input_lines == 6:
 			for description in self.browser.find_elements_by_css_selector(description_selector):
 				self.new_descriptions.append(description.text)
@@ -108,14 +114,18 @@ class Generator:
 	def error_no_new_articles(self):
 		''' Check if any new information is found, if not. Exit and
 		inform the user. '''
+
 		if len(self.new_titles) == 0 or len(self.new_links) == 0:
+
 			print("No new articles, exiting!")
+
 			self.browser.quit()
 			sys.exit(2)
 	
 	def generate_feed_entry(self):
 		''' This just does the work of actually writing the .xml file
 		with the extracted information. Nothing spectacular. '''
+
 		entry=open(self.channel_title.replace(" ","") + ".xml","w")
 		entry.write('<?xml version="1.0" encoding="UTF-8" ?>\n')
 		entry.write('<rss version="2.0">\n')
@@ -123,8 +133,10 @@ class Generator:
 		entry.write("  <title>" + self.channel_title + "</title>\n")
 		entry.write("  <link>" + self.channel_link + "</link>\n")
 		entry.write("  <description>" + self.channel_description + "</description>\n")
+
 		i=0
 		num_of_entries=len(self.new_titles)
+
 		while i<num_of_entries:
 			entry.write("  <item>\n")
 			entry.write("    <title>" + self.new_titles[i] + "</title>\n")
@@ -137,27 +149,29 @@ class Generator:
 			i+=1
 		entry.write("</channel>\n")
 		entry.write("</rss>")
+
 		self.browser.quit()
 	
 	def archive_new_feeds(self):
+
 		links=open("links","a")
 		titles=open("titles","a")
+
 		for link in self.new_links:
 			links.write(link + '\n')
+
 		for title in self.new_titles:
 			titles.write(title + '\n')
+
 		links.close()
 		titles.close()
 
 def main():
-	if read_from_stdin_flag is True:
-		title=stdin_lines[0]
-		link=stdin_lines[1]
-		description=stdin_lines[2]
-	else:
-		title=input("Enter the feed title: ")
-		link=input("Enter the feed link: ")
-		description=input("Enter the feed description: ")
+
+	title=stdin_lines[0]
+	link=stdin_lines[1]
+	description=stdin_lines[2]
+
 	generator=Generator(title,link,description)
 	generator.find_new_articles()
 	generator.error_no_new_articles()
